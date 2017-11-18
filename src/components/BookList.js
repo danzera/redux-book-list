4cb0b61151ab73fdf15ce9639f4fab9af8848828
Dan Zera <dczera@gmail.com>
2017-11-17 23:44:12 -0600

    promote our <BookList /> component to a container via the react-redux connect function

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
 	render() {
+		if (this.props.bookList) {
+			return <div>{this.props.bookList[0].title}</div>
+		}
+
 		return (
 			<div>BookList</div>
 		);
 	}
 }
+
+function mapStateToProps(state) {
+	return {
+		bookList: state.bookList
+	};
+}
+
+export default connect(mapStateToProps)(BookList);
